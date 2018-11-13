5b77208472647c9445bd35aca9714b19deb846ae
Christina <kristina3531@gmail.com>
2018-11-13 12:04:54 +0200

    add active class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	var filterizd = $('.filtr-container').filterizr({
-   //options object
-});
+	var filterizd = $('.filtr-container').filterizr({});
+	$(".filterListItem").on("click", function() {
+		$(".filterListItem").removeClass("active");
+		$(this).addClass("active");
+	});
 });
